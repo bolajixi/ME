2a6da8aade6dd8fefaa084f02e675f7136205df3
Bolaji Olawale <bolaji.olawale007@gmail.com>
2023-04-30 06:40:12 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
 # Install Git, Docker && Docker-compose
-sudo apt-get update -y install git docker.io docker-compose -y 
+sudo apt-get -y install git docker.io docker-compose
 
-# Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# Verify that Docker and Docker Compose are installed
+docker --version
+docker-compose --version
+
+# Install latest version of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl --silent https://github.com/docker/compose/releases/latest | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 docker-compose --version
-docker --version
-git --version
-
-# Get the Scripts
-git clone https://github.com/bolajixi/ME.git MediaEngine
-
-# Change into the director
-cd MediaEngine
 
 # Deploy the application using Docker Compose
 sudo docker-compose -f restream-app.yml up -d 
