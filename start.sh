782a6e3f52690885cd12daf8a2fbd3e28cdfb2de
Bolaji Olawale <bolaji.olawale007@gmail.com>
2023-04-30 07:08:33 +0100

    remove git

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Install Git, Docker && Docker-compose
-sudo apt-get -y install git docker.io docker-compose
+# Install Docker && Docker-compose
+sudo apt-get -y install docker.io docker-compose
 
 # Verify that Docker and Docker Compose are installed
 docker --version
 docker-compose --version
 
 # Install latest version of Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl --silent https://github.com/docker/compose/releases/latest | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/$(curl --silent https://github.com/docker/compose/releases/latest | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-docker-compose --version
+# docker-compose --version
 
 # Deploy the application using Docker Compose
 sudo docker-compose -f restream-app.yml up -d 
