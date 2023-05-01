ddd8c2e457de1a4dd7f7f809cc9e974bf2a42843
Bolaji Olawale <bolaji.olawale007@gmail.com>
2023-05-01 14:13:30 +0100

    clean up

--- start.sh
+++ start.sh
@@ -10,17 +10,8 @@ sudo apt-get -y install docker.io docker-compose
 docker --version
 docker-compose --version
 
-# Install latest version of Docker Compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/$(curl --silent https://github.com/docker/compose/releases/latest | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# docker-compose --version
-
 # Deploy the application using Docker Compose
 sudo docker-compose -f restream-app.yml up -d 
 
 # Verify that the container is up and running
 docker ps
-
-# docker run -p 1935:1935 MediaEngine
