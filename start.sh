3c5a54387482c79fa32d6ea6c0ba2abac61e8995
Mobolaji Olawale <mobolaji.olawale@flutterwavego.com>
2023-05-06 13:11:31 +0100

    Add stunnel config

--- start.sh
+++ start.sh
@@ -14,20 +14,20 @@ docker-compose --version
 sudo apt-get install stunnel4
 
 # Configure stunnel
-sudo cp stunnel.conf /etc/stunnel/stunnel.conf
 sudo sed -i '5iENABLED=1' /etc/default/stunnel4
-
-# Prepare SSL certificate
-# sudo openssl req -new -x509 -days 1000 -nodes -out /etc/ssl/certs/stunnel.pem -keyout /etc/ssl/certs/stunnel.pem
-
-# sudo openssl x509 -outform pem -in /etc/ssl/certs/stunnel.pem -out /etc/ssl/certs/stunnel.crt
-# sudo openssl rsa -outform pem -in /etc/ssl/certs/stunnel.pem -out /etc/ssl/certs/stunnel.key
-
-# Start stunnel service
-sudo systemctl start stunnel4
+sudo cp stunnel.conf /etc/stunnel/stunnel.conf
 
 # Deploy the application using Docker Compose
 sudo docker-compose -f restream-app.yml up -d 
+
+# Start stunnel service
+# sudo systemctl start stunnel4
+
+# Enable stunnel after boot
+sudo systemctl enable stunnel4.service
+
+# Restart stunnel
+sudo systemctl restart stunnel4.service
 
 # Verify that the container is up and running
 docker ps
